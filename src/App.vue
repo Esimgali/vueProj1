f1a9980136d00f29e853402c1ba370ff42ca93f8
Esimgali <esimgalikhamitov2005@gmail.com>
2023-10-19 16:32:57 +0600

    posts

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    Счетчик  {{count}}
+  </div>
+  <button @click="count++"> inc</button>
+  <div class="postsForm">
+    <input class="input" type="text" v-model="inputValue" @input="inputText"  @keyup.shift.enter="addToList"/>
+    <div id="inputText"></div>
+    <button class="submit" @click="addToList">Add post</button>
+    <ul class="postList"> 
+      <li class="postItem" v-for="(post, idx) in list" :key="post.key">
+        <div class="title">[{{ idx }}] {{ post.title }}</div>
+        <input type="text" @click.stop ref="postInput"/>
+        <button class="delete" @click="deletePost(idx)">Delete</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){   
+    return{ 
+      count: 0,
+      list: [],
+      inputValue: ""
+    }
+  },
+  methods: {
+      inc(){this.count++},
+      addToList(){
+        console.log(this.inputValue);
+        this.list.push({title: this.inputValue,key:new Date().toJSON()})
+        this.inputValue = ""
+      },
+      inputText(event){
+        console.log(event.target.value)
+        document.getElementById("inputText").innerHTML = event.target.value;
+      },
+      deletePost(idx){
+        console.log(idx, 1)
+        this.list.splice(idx, 1)
+      }
   }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
